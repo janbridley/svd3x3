c7a04668133358e6594506f4b3785e96944b5f58
janbridley <bradleyjenj@gmail.com>
2025-08-01 17:59:11 -0500

    Update tests

--- svd3x3/svd3.cpp
+++ svd3x3/svd3.cpp
@@ -8,32 +8,38 @@ namespace nb = nanobind;
 
 using namespace nb::literals;
 
-using Matrix33 = nb::ndarray<double, nb::numpy, nb::shape<3, 3>>;
+using Matrix33d = nb::ndarray<double, nb::numpy, nb::shape<3, 3>>;
+using Quaternion = nb::ndarray<double, nb::numpy, nb::shape<4>>;
 
 NB_MODULE(_c, m) {
   m.def("add", [](int a, int b) { return a + b; }, "a"_a, "b"_a);
   m.def("rsqrt", &rsqrt);
-  m.def("mul_a_b", [](const Matrix33 a, const Matrix33 b) {
+  m.def("mul_a_b", [](const Matrix33d a, const Matrix33d b) {
     double m[3][3];
     matmul(reinterpret_cast<double(*)[3]>(a.data()),
            reinterpret_cast<double(*)[3]>(b.data()), m);
-    return Matrix33(m).cast();
+    return Matrix33d(m).cast();
   });
-  m.def("mul_at_b", [](const Matrix33 a, const Matrix33 b) {
+  m.def("mul_at_b", [](const Matrix33d a, const Matrix33d b) {
     double m[3][3];
     matTmul(reinterpret_cast<double(*)[3]>(a.data()),
-           reinterpret_cast<double(*)[3]>(b.data()), m);
-    return Matrix33(m).cast();
+            reinterpret_cast<double(*)[3]>(b.data()), m);
+    return Matrix33d(m).cast();
+  });
+  m.def("q2mat3", [](const Quaternion q) {
+    double m[3][3];
+    quatToMat3(q.data(), m);
+    return Matrix33d(m).cast();
   });
   m.def(
       "svd",
-      [](const Matrix33 a) -> std::tuple<Matrix33, Matrix33, Matrix33> {
+      [](const Matrix33d a) -> std::tuple<Matrix33d, Matrix33d, Matrix33d> {
         // TODO: remove, this is just for debugging
         double u[3][3] = {-999, -999, -999, -999, -999, -999, -999, -999, -999};
         double s[3][3] = {-999, -999, -999, -999, -999, -999, -999, -999, -999};
         double v[3][3] = {-999, -999, -999, -999, -999, -999, -999, -999, -999};
         svd(reinterpret_cast<double(*)[3]>(a.data()), u, s, v);
-        return std::make_tuple(Matrix33(u), Matrix33(s), Matrix33(v));
+        return std::make_tuple(Matrix33d(u), Matrix33d(s), Matrix33d(v));
       },
       nb::rv_policy::automatic);
 }

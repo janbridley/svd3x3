427f3328c3e8a3ba0e2ca42045016de21e92552a
janbridley <bradleyjenj@gmail.com>
2025-08-01 16:13:53 -0500

    test_svd->test_mat

---
+++ tests/test_mat.py
@@ -0,0 +1,19 @@
+import pytest
+from hypothesis import given, settings, HealthCheck
+from conftest import generate_random_matrixes, nonsingular_3x3_matrices
+import numpy as np
+from svd3x3._c import mul_a_b, mul_at_b
+
+N = 20
+
+
+@pytest.mark.parametrize("a", generate_random_matrixes(N))
+@pytest.mark.parametrize("b", generate_random_matrixes(N))
+def test_matmul(a, b):
+    np.testing.assert_allclose(mul_a_b(a[:], b[:]), a @ b)
+
+
+@pytest.mark.parametrize("a", generate_random_matrixes(N))
+@pytest.mark.parametrize("b", generate_random_matrixes(N))
+def test_matmul_transposed(a, b):
+    np.testing.assert_allclose(mul_at_b(a[:], b[:]), a.T @ b)

6b6f1d11d3ac3703fd523e414aea63d5b3c0e09e
janbridley <bradleyjenj@gmail.com>
2025-08-01 17:03:56 -0500

    Export as tuple

--- src/svd3.cpp
+++ src/svd3.cpp
@@ -1,27 +1,36 @@
 #include "svd3.hpp"
-#include <array>
-#include <iostream>
 #include <nanobind/nanobind.h>
 #include <nanobind/ndarray.h>
+#include <nanobind/stl/tuple.h>
 #include <tuple>
 
 namespace nb = nanobind;
 
 using namespace nb::literals;
 
-using NBMatrix33 = nb::ndarray<double, nb::numpy, nb::shape<3, 3>>;
+using Matrix33 = nb::ndarray<double, nb::numpy, nb::shape<3, 3>>;
 
 NB_MODULE(_c, m) {
   m.def("add", [](int a, int b) { return a + b; }, "a"_a, "b"_a);
   m.def("rsqrt", &rsqrt);
-  m.def("mul_a_b", [](const NBMatrix33 a, const NBMatrix33 b) {
+  m.def("mul_a_b", [](const Matrix33 a, const Matrix33 b) {
     double m[3][3];
     matmul(a.data(), b.data(), m);
-    return NBMatrix33(m).cast();
+    return Matrix33(m).cast();
   });
-  m.def("mul_at_b", [](const NBMatrix33 a, const NBMatrix33 b) {
+  m.def("mul_at_b", [](const Matrix33 a, const Matrix33 b) {
     double m[3][3];
     matTmul(a.data(), b.data(), m);
-    return NBMatrix33(m).cast();
+    return Matrix33(m).cast();
   });
+  m.def(
+      "svd",
+      [](const Matrix33 a) -> std::tuple<Matrix33, Matrix33, Matrix33> {
+        // TODO: remove, this is just for debugging
+        double u[3][3] = {-999, -999, -999, -999, -999, -999, -999, -999, -999};
+        double s[3][3] = {-999, -999, -999, -999, -999, -999, -999, -999, -999};
+        double v[3][3] = {-999, -999, -999, -999, -999, -999, -999, -999, -999};
+        svd(a.data(), u, s, v);
+        return std::make_tuple(Matrix33(u), Matrix33(s), Matrix33(v));
+      });
 }

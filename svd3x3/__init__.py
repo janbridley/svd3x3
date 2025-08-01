77e4ddfe6f7bbf72fe5bb048f0b46897d985cc0c
janbridley <bradleyjenj@gmail.com>
2025-08-01 09:13:00 -0500

    init

---
+++ svd3x3/__init__.py
@@ -0,0 +1,6 @@
+"""Fast SVD for 3x3 matrices.
+
+This is an implementation of the method described in [Computing the Singular Value Decomposition of 3x3 matrices with minimal branching and elementary floating point operations](http://pages.cs.wisc.edu/~sifakis/papers/SVD_TR1690.pdf), adapted from
+code by Eric Jang. This version of the library includes Python bindings for ease of use
+and testing.
+"""

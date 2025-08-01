27cf40e7986f2f1e6777987808bc59d3d76d40cb
janbridley <bradleyjenj@gmail.com>
2025-08-01 17:03:46 -0500

    Add SVD test

--- tests/test_mat.py
+++ tests/test_mat.py
@@ -2,7 +2,7 @@ import pytest
 from hypothesis import given, settings, HealthCheck
 from conftest import generate_random_matrixes, nonsingular_3x3_matrices
 import numpy as np
-from svd3x3._c import mul_a_b, mul_at_b
+from svd3x3._c import mul_a_b, mul_at_b, svd
 
 N = 20
 
@@ -17,3 +17,11 @@ def test_matmul(a, b):
 @pytest.mark.parametrize("b", generate_random_matrixes(N))
 def test_matmul_transposed(a, b):
     np.testing.assert_allclose(mul_at_b(a[:], b[:]), a.T @ b)
+
+@pytest.mark.parametrize("a", generate_random_matrixes(N**2))
+def test_svd(a):
+    ref_u, ref_s, ref_v = np.linalg.svd(a[:])
+    u, s, v = svd(a[:])
+    # np.testing.assert_allclose(u, ref_u)
+    np.testing.assert_allclose(s.round(13), np.diag(ref_s))
+    # np.testing.assert_allclose(u, ref_u)

bea8cb1740cb55ad350a5aaaa2752ea880ff314b
janbridley <bradleyjenj@gmail.com>
2025-08-03 14:35:12 -0500

    Update tests and docs

--- svd3x3/__init__.py
+++ svd3x3/__init__.py
@@ -6,34 +6,21 @@ and testing.
 """
 
 import numpy as np
+from numpy.typing import ArrayLike
 from ._c import svd as _svd
+from ._bench import _bench
 
 
-def svd(a):
+def svd3(a: ArrayLike) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
+    """Compute the singular value decomposition `U @ S @ VT` of a 3x3 matrix.
+
+    The calculation is performed in double precision, and is typically ~65% faster than
+    the same operation using Numpy.
+    """
     a = np.asarray(a)
-    if a.shape != (3,3):
+    if a.shape != (3, 3):
         raise ValueError("Input matrix does not have the correct shape.")
     return _svd(a)
 
-def _bench(N = 500_000, N_REPS=20):
-    import numpy as np
-    import timeit
-    from ._c import svd
 
-    S_TO_US = 1e6
-
-    def bench(fn):
-        matrices = np.random.rand(N, 3, 3)
-        def svd_all():
-            for A in matrices:
-                fn(A)
-        return timeit.repeat(svd_all, number=1, repeat=N_REPS)
-
-    for mode, fn in [("np ", np.linalg.svd), ("jen", svd)]:
-        times = bench(fn)
-
-        avg_times = [(t / N) * S_TO_US for t in times]
-        mean = np.mean(avg_times)
-        stdev = np.std(avg_times)
-
-        print(f"Average SVD time {mode}: {mean:.4f} ± {stdev:.4f} μs")
+__all__ = ["svd3", "_bench"]
--- svd3x3/_bench.py
+++ svd3x3/_bench.py
@@ -1,1 +1,24 @@
+def _bench(N=500_000, N_REPS=20):
+    import numpy as np
+    import timeit
+    from ._c import svd
 
+    S_TO_US = 1e6
+
+    def bench(fn):
+        matrices = np.random.rand(N, 3, 3)
+
+        def svd_all():
+            for A in matrices:
+                fn(A)
+
+        return timeit.repeat(svd_all, number=1, repeat=N_REPS)
+
+    for mode, fn in [("np ", np.linalg.svd), ("jen", svd)]:
+        times = bench(fn)
+
+        avg_times = [(t / N) * S_TO_US for t in times]
+        mean = np.mean(avg_times)
+        stdev = np.std(avg_times)
+
+        print(f"Average SVD time {mode}: {mean:.4f} ± {stdev:.4f} μs")
